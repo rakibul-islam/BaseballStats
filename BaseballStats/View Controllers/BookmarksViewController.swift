c4a13dbdf6f789281c6995ade60013477e369698
Rakibul Islam <ricky@solaritsolutions.com>
2018-03-04 17:46:14 -0600

    navigate from bookmark VC to player VC

--- BaseballStats/View Controllers/BookmarksViewController.swift
+++ BaseballStats/View Controllers/BookmarksViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class BookmarksViewController: UITableViewController {
     var players = [PlayerMO]()
-    var coreDataController = CoreDataController.sharedInstance
+    var selectedPlayer: PlayerMO?
+    lazy var playerRetrievalUtility = PlayerRetrievalUtility()
+    lazy var coreDataController = CoreDataController.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let statsVC = segue.destination as? PlayerStatsViewController {
+            statsVC.player = selectedPlayer
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,5 +48,36 @@ class BookmarksViewController: UITableViewController {
         tableViewCell.textLabel?.text = player.displayName
         tableViewCell.detailTextLabel?.text = player.positionName
         return tableViewCell
+    }
+    
+    //MARK: - Table view delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPlayer = players[indexPath.row]
+        if let battingStats = selectedPlayer?.battingStats, battingStats.count > 0 {
+            performSegue(withIdentifier: "showPlayerInfo", sender: nil)
+        } else if let pitchingStats = selectedPlayer?.pitchingStats, pitchingStats.count > 0 {
+            performSegue(withIdentifier: "showPlayerInfo", sender: nil)
+        } else {
+            getStatsForSelectedPlayer() //Should never get here, but just in case...
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Other methods
+    func getStatsForSelectedPlayer() {
+        CommonAlerts.sharedInstance.showLoadingAlertOn(viewController: self)
+        playerRetrievalUtility.getStats(for: selectedPlayer, completionBlock: {
+            DispatchQueue.main.async {
+                CommonAlerts.sharedInstance.dismissLoadingAlert(completionBlock: {
+                    self.performSegue(withIdentifier: "showPlayerInfo", sender: nil)
+                })
+            }
+        }, failureBlock: { (error) in
+            DispatchQueue.main.async {
+                CommonAlerts.sharedInstance.dismissLoadingAlert(completionBlock: {
+                    CommonAlerts.showErrorAlertOn(viewController: self, messageString: nil, error: error)
+                })
+            }
+        })
     }
 }
